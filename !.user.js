cc2c71f79037cec6027d638e9e64e7918d20241f
Jérémy Bézairie <Jeremy_Bezairie@outlook.com>
2024-09-01 15:27:59 +0000

    Add files via upload

---
+++ !.user.js
@@ -0,0 +1,7 @@
+// ==UserScript== 
+// @match       https://www.youtube.com/watch?v=*
+// ==UserScript== 
+document.getElementsByClassName('ytp-gradient-top')[0].remove()
+document.getElementsByClassName('ytp-chrome-top')[0].remove()
+document.getElementsByClassName('ytp-gradient-bottom')[0].remove()
+document.getElementsByTagName('video')[0].onpause=()=>document.getElementsByClassName('ytp-bezel-text-hide')[0].remove()
