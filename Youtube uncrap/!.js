9aeff27c1a1829419ecc543d7ee04706aeec533a
Jérémy Bézairie <Jeremy_Bezairie@outlook.com>
2024-11-05 10:56:13 +0000

    Delete Youtube uncrap directory

--- Youtube uncrap/!.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByClassName('ytp-gradient-top')[0].remove()
-document.getElementsByClassName('ytp-chrome-top')[0].remove()
-document.getElementsByClassName('ytp-gradient-bottom')[0].remove()
-document.getElementsByTagName('video')[0].onpause=()=>document.getElementsByClassName('ytp-bezel-text-hide')[0].remove()
